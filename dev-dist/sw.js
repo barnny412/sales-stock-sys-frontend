140cc5f193cdc046b048b11dad8f95fac67d69f4
Barnny412 <barnny412@gmail.com>
2025-04-13 18:14:25 +0200

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dp8aar0kiu8"
+    "revision": "0.2m61k6a8vm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
